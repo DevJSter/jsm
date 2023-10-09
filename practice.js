fc31cb52d18370426cc8d65b4f892506a93cc69f
Shubham Tiwari <shubht3303@gmail.com>
2023-10-09 23:45:21 +0530

    Update

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
-const shubham="Hello ji Kaise ho saare";
-const shubham1="\tShubham here";
-const shubham2=" from Thane";
-console.log(shubham);
-console.log(shubham.concat(shubham1,shubham2));
-cost=shubham.toUpperCase;
+// const shubham="Hello ji Kaise ho saare";
+// const shubham1="\tShubham here";
+// const shubham2=" from Thane";
+// console.log(shubham);
+// console.log(shubham.concat(shubham1,shubham2));
+// cost=shubham.toUpperCase;
