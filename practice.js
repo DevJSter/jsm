79f8adbb44ccc3969f3736ece5ddf6fa6fbe6195
Shubham Tiwari <shubht3303@gmail.com>
2023-10-10 17:31:33 +0530

    Strings

--- practice.js
+++ practice.js
@@ -1,6 +1,37 @@
-const shubham="Hello ji Kaise ho saare";
-const shubham1="\tShubham here";
-const shubham2=" from Thane";
-console.log(shubham);
-console.log(shubham.concat(shubham1,shubham2));
-cost=shubham.toUpperCase;
+// const shubham="Hello ji Kaise ho saare";
+// const shubham1="\tShubham here";
+// const shubham2=" from Thane";
+// console.log(shubham);
+// console.log(shubham.concat(shubham1,shubham2));
+// cost=shubham.toUpperCase;
+
+// let VariableName = "Welcome";
+
+// console.log("Hello",VariableName);
+// console.log("Test");
+
+// String methods :: Defining strings can be done by three ways in JS
+/* First is using  singleQuotes eg ''  
+const singleQuotes = 'Single Quotes only';
+console.log(singleQuotes); //The output will be Single Quotes only
+
+2nd way is using  doubleequotesQuotes eg  ""
+const doubleQuotes = "Double Quotes only";
+console.log(doubleQuotes); //The output will be Double Quotes Only
+
+Third is using Backticks eg ``  
+
+const backticks = `Back Ticks`;
+console.log(backticks);  //The output will be Back Ticks
+
+
+Backticks is most use because we can add javascript i.e we can embedded method i.e embed any else variable inside of it(Reusability of any variable) even when we are declaring any Variable for eg
+
+For eg:
+const var1 = `Shubham`;
+
+const var2 = `Hello ${var1}`;
+
+console.log(var2);  //The output will be Hello Shubham
+
+*/
