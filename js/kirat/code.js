78e1cd215a32123e97d7992cc98bf3150eb8eb0d
Shubham Tiwari <shubht3303@gmail.com>
2024-01-06 17:52:03 +0530

    checkpoints

--- js/kirat/code.js
+++ js/kirat/code.js
@@ -1,16 +1,29 @@
-function startLane(n) {
- var answer = "";
- for (var i = 0; i < n; i++) {
-  answer += "**";
- }
- console.log(answer) ;
-}
+// function startLane(n) {
+//  var answer = "";
+//  for (var i = 0; i < n; i++) {
+//   answer += "**";
+//  }
+//  console.log(answer) ;
+// }
+
+// function starryPattern(n) {
+//  for (var i = 1; i <= n; i++) {
+//   startLane(i);
+//  }
+// }
+
+// var answer = starryPattern(10);
+// console.log(answer);
 
 function starryPattern(n) {
- for (var i = 1; i <= n; i++) {
-  startLane(i);
+  for (var i = 1; i <= n; i++) {
+   var answer = "";
+    for (var j = 0; j < i+1; j++) {
+      answer += "**";
+     }
+      console.log(answer) ;
+  }
  }
-}
-
-var answer = starryPattern(10);
-console.log(answer);
+ 
+ var answer = starryPattern(10);
+ console.log(answer);
