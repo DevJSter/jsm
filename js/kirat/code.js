7a4faad39aa7a92813f448cef15e7afe647ead99
Shubham Tiwari <shubht3303@gmail.com>
2024-01-06 18:01:18 +0530

    checkpoints

--- js/kirat/code.js
+++ js/kirat/code.js
@@ -31,3 +31,15 @@
 
 // Javascript is Single threaded
 
+function Complexfn(n) {
+ var sum =0;
+ for (var i=0; i<n; i++) {
+  sum += i;
+ }
+ return sum;
+}
+
+var t1 = performance.now();
+var ans = Complexfn(1000000000);
+
+console.log(ans);
