c5455cce4534a28a2587965107fdb2a7330d895b
Shubham Tiwari <shubht3303@gmail.com>
2024-01-06 23:18:19 +0530

    setTimeout

--- js/kirat/code.js
+++ js/kirat/code.js
@@ -24,7 +24,7 @@
 //       console.log(answer) ;
 //   }
 //  }
- 
+
 //  var answer = starryPattern(10);
 //  console.log(answer);
 
@@ -49,7 +49,7 @@
 //  console.log("Radha");
 // }
 
-function count (first, second) {
+function count(first, second) {
  var sum = 0;
  for (var i = first; i < second; i++) {
   sum += i;
@@ -57,10 +57,11 @@ function count (first, second) {
  return sum;
 }
 
-setTimeout(function() {
+setTimeout(function () {
  console.log("10 Seconds have passed!");
-},1000)
+}, 1000)
 
 var ans = count(1, 100);
 console.log(ans);
 //so what happened here? Basically, the setTimeout function is a function that takes in two parameters: a function and a time in milliseconds. The function is the function that you want to execute after the time has passed. The time is the time in milliseconds that you want to wait before executing the function. So, in this case, we want to wait 10 seconds before executing the function that prints out the message. So, we pass in a function that prints out the message and we pass in 10000 milliseconds, which is 10 seconds. So, after 10 seconds, the function will execute and print out the message.
+
