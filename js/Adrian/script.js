64fb1aeae5cb857fa61e8a3a647bbc8a6722003f
Shubham Tiwari <shubht3303@gmail.com>
2024-01-07 02:04:35 +0530

    add : DOM

---
+++ js/Adrian/script.js
@@ -0,0 +1,31 @@
+// The 'new'  keyword 
+// 1. It creates a new empty object {}
+
+
+// const perosn  = {};
+// const oerson1 = new Object();
+
+// const Date = new Date();
+// console.log(Date); 
+
+
+// the this  kewyword 
+// 1. Gives methods access to their object
+// 2. Execute the same code for multiple objects
+
+// const perosn  = {
+//     name: 'Adrian',
+//     walk() {
+//         console.log(this);
+//     }
+
+// }; 
+
+// perosn.walk();
+
+// const walk = perosn.walk.bind(perosn);
+// walk();
+
+// const square = function(number) {
+//     return number * number;
+// }
