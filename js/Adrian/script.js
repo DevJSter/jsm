3a2b4d81513432a675c9412082f71767555357a5
Shubham Tiwari <shubht3303@gmail.com>
2024-01-07 02:14:12 +0530

    add : classes

--- js/Adrian/script.js
+++ js/Adrian/script.js
@@ -29,3 +29,30 @@
 // const square = function(number) {
 //     return number * number;
 // }
+
+// const square = number => number * number;
+// console.log(square(5));
+
+// const jobs = [
+//     { id: 1, isActive: true },
+//     { id: 2, isActive: true },
+//     { id: 3, isActive: false },
+// ];
+
+//classes
+
+const person = {
+    name: 'Adrian',
+    walk() {
+        console.log(this);
+    }
+};
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    walk() {
+        console.log("walk");
+    }
+}
