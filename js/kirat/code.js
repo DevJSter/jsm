b185422277d14b7a993c1bad45cb0fab84d70270
Shubham Tiwari <shubht3303@gmail.com>
2024-01-06 17:52:49 +0530

    checkpoints

--- js/kirat/code.js
+++ js/kirat/code.js
@@ -15,15 +15,17 @@
 // var answer = starryPattern(10);
 // console.log(answer);
 
-function starryPattern(n) {
-  for (var i = 1; i <= n; i++) {
-   var answer = "";
-    for (var j = 0; j < i+1; j++) {
-      answer += "**";
-     }
-      console.log(answer) ;
-  }
- }
+// function starryPattern(n) {
+//   for (var i = 1; i <= n; i++) {
+//    var answer = "";
+//     for (var j = 0; j < i+1; j++) {
+//       answer += "**";
+//      }
+//       console.log(answer) ;
+//   }
+//  }
  
- var answer = starryPattern(10);
- console.log(answer);
+//  var answer = starryPattern(10);
+//  console.log(answer);
+
+// 
