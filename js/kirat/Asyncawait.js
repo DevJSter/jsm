68ee7fbe80884a71f5be1b82d7a94191111ce9a4
Shubham Tiwari <shubht3303@gmail.com>
2024-01-16 01:24:58 +0530

    Remove unused code

---
+++ js/kirat/Asyncawait.js
@@ -0,0 +1,5 @@
+// To avoid callback hell, we use async await
+//More simpler ways are also there to avoid callback hell like promises, generators etc
+//Async await is a syntactic sugar over promises
+
+
